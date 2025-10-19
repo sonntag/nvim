5291a385598c871824915735cf5b5034850c275e
Justin Sonntag <justin.sonntag@gmail.com>
2025-10-19 16:01:55 -0700

    Add more of my options

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,15 +1,20 @@
 vim.g.autoformat = true
 
+vim.o.autoindent = true
 vim.o.confirm = true
+vim.o.expandtab = true -- no tabs!
 vim.o.ignorecase = true
 vim.o.inccommand = "split"
 vim.o.list = true
 vim.o.mouse = "a"
+vim.o.scrolloff = 4 -- makes sure the cursor is buffered by 4 lines when scrolling
+vim.o.shiftwidth = 4
 vim.o.showmode = false
 vim.o.signcolumn = "yes"
 vim.o.smartcase = true
 vim.o.splitbelow = true
 vim.o.splitright = true
+vim.o.tabstop = 4
 vim.o.timeoutlen = 300
 vim.o.undofile = true
 vim.o.updatetime = 250
