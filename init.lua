1db185ab285fd579dd8bc5a691654ad5a7b264ae
Justin Sonntag <justin.sonntag@gmail.com>
2025-10-20 14:35:11 -0700

    Add optional profiling of nvim startup

--- init.lua
+++ init.lua
@@ -1,3 +1,12 @@
+if vim.env.PROF then
+    vim.cmd.packadd("snacks.nvim")
+    require("snacks.profiler").startup({
+        startup = {
+            event = "VimEnter", -- stop profiler on this event. Defaults to `VimEnter`
+        },
+    })
+end
+
 require("autocommands")
 require("keymaps")
 require("options")
